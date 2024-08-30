ca2cc61f7d52b7cf1c381b619c9009ea9065ff55
GabrielFilomeno <biel_floripa@hotmail.com>
2024-08-30 20:08:14 -0300

    rotas

--- LocadoraAutomoveis/src/app/app.routes.ts
+++ LocadoraAutomoveis/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ListagemAutomoveisComponent } from './pages/listagem-automoveis/listagem-automoveis.component';
+import { DetalhesAutomovelComponent } from './pages/detalhes-automovel/detalhes-automovel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListagemAutomoveisComponent
+    },
+    {
+        path: 'listagem',
+        component: ListagemAutomoveisComponent
+    },
+    {
+        path: 'detalhes',
+        component: DetalhesAutomovelComponent
+    }
+];
